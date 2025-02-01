fa74b664a393f5772c18215682467ef31e610ec9
Yerassy11 <y_khudaibergen@kbtu.kz>
2025-02-01 10:20:55 +0500

    Add files via upload

---
+++ lab1/task 1.4.py
@@ -0,0 +1,8 @@
+#This is comment 
+#This is also comments :)
+print("Hello teacher")  #<-this is not
+""" 
+Hi
+My
+Friend 
+""" 
---
+++ lab1/task1.2.py
@@ -0,0 +1,3 @@
+#Python:Get Started
+import sys 
+print (sys.version) 
